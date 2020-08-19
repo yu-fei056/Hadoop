72d0690ef2c74f7610fb7c7360bbacca4c0bbfef
yu-fei056 <1904084356@qq.com>
2020-08-19 14:19:16 +0800

    hadoop

---
+++ Mapreduce/src/main/java/com/yufei/inputformat/CustomIFMapper.java
@@ -0,0 +1,17 @@
+package com.yufei.inputformat;
+
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class CustomIFMapper extends Mapper<Text, BytesWritable, Text, BytesWritable>{
+
+    @Override
+    protected void map(Text key, BytesWritable value,			Context context)		throws IOException, InterruptedException {
+
+        context.write(key, value);
+    }
+}
+
---
+++ Mapreduce/src/main/java/com/yufei/inputformat/WholeFileInputformat.java
@@ -0,0 +1,31 @@
+package com.yufei.inputformat;
+
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+// 定义类继承FileInputFormat
+public class WholeFileInputformat extends FileInputFormat<Text, BytesWritable>{
+
+    @Override
+    protected boolean isSplitable(JobContext context, Path filename) {
+        return false;
+    }
+
+    @Override
+    public RecordReader<Text, BytesWritable> createRecordReader(InputSplit split, TaskAttemptContext context)	throws IOException, InterruptedException {
+
+        WholeRecordReader recordReader = new WholeRecordReader();
+        recordReader.initialize(split, context);
+
+        return recordReader;
+    }
+}
+
